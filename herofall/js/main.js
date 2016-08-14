ee3e09b5cbcade6f1fdf1f274de316686c812e19
qqqqw123777 <403830986@qq.com>
2016-08-15 00:05:07 +0800

    5版预览
    ok

--- herofall/js/main.js
+++ herofall/js/main.js
@@ -1,16 +1,37 @@
 var li = $("li");
 var card = $(".card");
+var top = li.length;
+var cards = 4;
+
 
 window.onload = function(){
+	if(window.innerWidth > 1551){
+		cards = 4;
+	}else if(window.innerWidth < 1551 && window.innerWidth > 1115){
+		cards = 3;
+	}else if(window.innerWidth < 1115 && window.innerWidth > 783){
+		cards = 2;
+	}else if(window.innerWidth < 783){
+		cards = 1;
+	}
   var s = Math.floor(window.innerWidth / li.length) - 10;
-  var w = Math.floor(window.innerWidth / 4) - 10;
+  var w = Math.floor(window.innerWidth / cards) - 10;
   $("li").css("width", s + "px");
   $(".card").css("width", w + "px");
 }
 
 window.onresize = function(){
+	if(window.innerWidth > 1551){
+		cards = 4;
+	}else if(window.innerWidth <1551 && window.innerWidth > 1115){
+		cards = 3;
+	}else if(window.innerWidth < 1115 && window.innerWidth > 783){
+		cards = 2;
+	}else if(window.innerWidth < 783){
+		cards = 1;
+	}
   var s = Math.floor(window.innerWidth / li.length) - 10;
-  var w = Math.floor(window.innerWidth / 4) - 10;
+  var w = Math.floor(window.innerWidth / cards) - 10;
   $("li").css("width", s + "px");
   $(".card").css("width", w + "px");
 }
